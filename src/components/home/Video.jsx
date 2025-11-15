e96acded6781195f3ec59b51ca3394e99b3504f0
ycode37 <ycode37@gmail.com>
2025-11-15 16:41:08 +0530

    commit

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="../../../public/video.mp4"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="/video.mp4"></video>
     </div>
   )
 }
